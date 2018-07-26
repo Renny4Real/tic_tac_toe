0275a52059132b66118048df618bc4808cfedf81
Renny Fadoju <rennyvbirds@yahoo.com>
2018-07-26 12:29:08 +0100

    Refactor code into guard clauses  and change name of check status to current status

--- lib/check_board.rb
+++ lib/check_board.rb
@@ -3,17 +3,13 @@
 class CheckBoard
   def initialize(player_gateway: player_gateway)
     @player_gateway = player_gateway
-    @score = ''
-    @check_score = @player_gateway.get_board
+    @status = ''
+    @current_status = @player_gateway.get_board
     @board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
   end
 
   def execute(*)
-    if @player_gateway.get_board.nil?
-      @board
-    else
-      print_score
-    end
+    @player_gateway.get_board.nil? ? @board : print_status
   end
 
   private
@@ -39,78 +35,73 @@ class CheckBoard
   end
 
   def diagonal_forward?(type)
-    @check_score[0][0] == type \
-    && @check_score[1][1] == type \
-    && @check_score[2][2] == type
+    @current_status[0][0] == type \
+    && @current_status[1][1] == type \
+    && @current_status[2][2] == type
   end
 
   def diagonal_backward?(type)
-    @check_score[0][2] == type \
-    && @check_score[1][1] == type \
-    && @check_score[2][0] == type
+    @current_status[0][2] == type \
+    && @current_status[1][1] == type \
+    && @current_status[2][0] == type
   end
 
   def vertical_left?(type)
-    @check_score[0][0] == type \
-    && @check_score[1][0] == type \
-    && @check_score[2][0] == type
+    @current_status[0][0] == type \
+    && @current_status[1][0] == type \
+    && @current_status[2][0] == type
   end
 
   def vertical_middle?(type)
-    @check_score[0][1] == type \
-    && @check_score[1][1] == type \
-    && @check_score[2][1] == type
+    @current_status[0][1] == type \
+    && @current_status[1][1] == type \
+    && @current_status[2][1] == type
   end
 
   def vertical_right?(type)
-    @check_score[0][2] == type \
-    && @check_score[1][2] == type \
-    && @check_score[2][2] == type
+    @current_status[0][2] == type \
+    && @current_status[1][2] == type \
+    && @current_status[2][2] == type
   end
 
   def horizontal_top?(type)
-    @check_score[0][0] == type \
-    && @check_score[0][1] == type \
-    && @check_score[0][2] == type
+    @current_status[0][0] == type \
+    && @current_status[0][1] == type \
+    && @current_status[0][2] == type
   end
 
   def horizontal_middle?(type)
-    @check_score[1][0] == type \
-    && @check_score[1][1] == type \
-    && @check_score[1][2] == type
+    @current_status[1][0] == type \
+    && @current_status[1][1] == type \
+    && @current_status[1][2] == type
   end
 
   def horizontal_bottom?(type)
-    @check_score[2][0] == type \
-    && @check_score[2][1] == type \
-    && @check_score[2][2] == type
+    @current_status[2][0] == type \
+    && @current_status[2][1] == type \
+    && @current_status[2][2] == type
   end
 
   def return_X_wins_status
-    { status: :X_wins }
+    @score = { status: :X_wins }
   end
 
   def return_O_wins_status
-    { status: :O_wins }
+    @score = { status: :O_wins }
   end
 
   def return_draw_status
-    { status: :draw }
+    @score = { status: :draw }
   end
 
-  def print_score
-    if win_X?
-      @score = return_X_wins_status
-    elsif win_O?
-      @score = return_O_wins_status
-    elsif count_dashes != 9
-      @check_score
-    else
-      @score = return_draw_status
-    end
+  def print_status
+      return return_X_wins_status if win_X?
+      return return_O_wins_status if win_O?
+      return @current_status if count_dashes != 9
+      return_draw_status
   end
 
   def count_dashes
-    9 - @check_score.flatten.count('-')
+    9 - @current_status.flatten.count('-')
   end
 end
