e89085afb845762ea2a1307bbeda7835325577e2
Renny Fadoju <rennyvbirds@yahoo.com>
2018-07-30 16:58:10 +0100

    Remove code

--- lib/player.rb
+++ lib/player.rb
@@ -1,19 +1,1 @@
 # frozen_string_literal: true
-
-class Player
-  def initialize(type, x_coordinate, y_coordinate)
-    @type = type
-    @x = x_coordinate
-    @y = y_coordinate
-  end
-
-  attr_reader :type
-
-  def x_coordinate
-    @x
-  end
-
-  def y_coordinate
-    @y
-  end
-end
