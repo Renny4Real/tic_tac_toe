3b826266defa2dc1b25cdbbfe7c86c1bda3065f6
Renny Fadoju <rennyvbirds@yahoo.com>
2018-08-06 16:03:55 +0100

    change name of check board file and class

---
+++ lib/check_game_condition.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+class CheckGameCondition
+  def initialize(file_board_gateway:)
+    @board = file_board_gateway.retrieve_board
+  end
+
+  def execute
+    return :X_wins if win?(:X)
+    return :O_wins if win?(:O)
+    return nil if count_dashes != 9
+    :draw
+  end
+
+  private
+
+  def win?(type)
+    diagonal?(type) || vertical?(type) || horizontal?(type)
+  end
+
+  def diagonal?(type)
+    diagonal_forward?(type) || diagonal_backward?(type)
+  end
+
+  def vertical?(type)
+    left_column?(type) || middle_column?(type) || right_column?(type)
+  end
+
+  def horizontal?(type)
+    top_row?(type) || middle_row?(type) || bottom_row?(type)
+  end
+
+  def diagonal_forward?(type)
+    [@board[0][0], @board[1][1], @board[2][2]] == [type, type, type]
+  end
+
+  def diagonal_backward?(type)
+    [@board[0][2], @board[1][1], @board[2][0]] == [type, type, type]
+  end
+
+  def left_column?(type)
+    [@board[0][0], @board[1][0], @board[2][0]] == [type, type, type]
+  end
+
+  def middle_column?(type)
+    [@board[0][1], @board[1][1], @board[2][1]] == [type, type, type]
+  end
+
+  def right_column?(type)
+    [@board[0][2], @board[1][2], @board[2][2]] == [type, type, type]
+  end
+
+  def top_row?(type)
+    [@board[0][0], @board[0][1], @board[0][2]] == [type, type, type]
+  end
+
+  def middle_row?(type)
+    [@board[1][0], @board[1][1], @board[1][2]] == [type, type, type]
+  end
+
+  def bottom_row?(type)
+    [@board[2][0], @board[2][1], @board[2][2]] == [type, type, type]
+  end
+
+  def return_X_wins_status
+    { status: :X_wins }
+  end
+
+  def return_O_wins_status
+    { status: :O_wins }
+  end
+
+  def return_draw_status
+    { status: :draw }
+  end
+
+  def return_no_status
+    { status: nil }
+  end
+
+  def count_dashes
+    9 - @board.flatten.count('-')
+  end
+end
---
+++ lib/file_board_gateway.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'json'
+
+class FileBoardGateway
+  attr_accessor :game_start
+
+  def initialize
+    @game_start = true
+  end
+
+  def retrieve_board
+    file = File.read('gamestate.json')
+    data = JSON.parse(file, symbolize_names: true)
+    return data if data.nil?
+    build_board(data.fetch(:board))
+  end
+
+  def save_board(board)
+    File.open('gamestate.json', 'w') do |file|
+      file.write({ board: board }.to_json)
+    end
+  end
+
+  def build_board(board)
+    board.each do |row|
+      row.each_index do |cell|
+        row[cell] = :X if row[cell] == 'X'
+        row[cell] = :O if row[cell] == 'O'
+      end
+    end
+  end
+end
