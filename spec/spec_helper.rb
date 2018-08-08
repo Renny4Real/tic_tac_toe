d5dd990c2d3128cae6be45d9dec99ce6d4de9fcf
Renny Fadoju <rennyvbirds@yahoo.com>
2018-08-08 12:34:54 +0100

    Configure Spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,11 +6,15 @@ require 'check_game_condition'
 require 'file_board_gateway'
 require 'AI'
 require 'rack/test'
+require 'UI'
+require "./environment"
 
 ENV['RACK_ENV'] = 'test'
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods
+  config.include RSpecHtmlMatchers
+  
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
