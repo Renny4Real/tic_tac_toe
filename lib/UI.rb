70ed119f0e78c8bec253e31d4ec4069485666968
Renny Fadoju <rennyvbirds@yahoo.com>
2018-08-13 09:28:14 +0100

    Change code styles

--- lib/UI.rb
+++ lib/UI.rb
@@ -7,26 +7,28 @@ require 'place_mark'
 require 'view_board'
 require 'check_game_condition'
 
+before do
+  @file_board_gateway = FileBoardGateway.new
+  @place_mark = PlaceMark.new(file_board_gateway: @file_board_gateway)
+  @view_board = ViewBoard.new(file_board_gateway: @file_board_gateway)
+  @game = @view_board.execute
+end
+
   get '/' do
     reset_board
     erb :setup
   end
 
   get '/play' do
-    @board = game_grid
-    @status = game_status
     @title = 'Tic-Tac-Toe'
-    erb :index
+    erb :index, locals: { board: game_grid, status: game_status }
   end
 
   post '/:row/:cell' do
-    @cell = params[:cell].to_i
-    @row = params[:row].to_i
-    @file_board_gateway = FileBoardGateway.new
-    @place_mark = PlaceMark.new(file_board_gateway: @file_board_gateway)
-    @place_mark.execute(player: :X, x: @row, y: @cell)
-    @status = game_status
-    @place_mark.execute(player: :O) if @status == nil
+    cell = params[:cell].to_i
+    row = params[:row].to_i
+    @place_mark.execute(player: :X, x: row, y: cell)
+    @place_mark.execute(player: :O) if game_status == nil
     redirect '/play'
   end
 
@@ -40,10 +42,7 @@ require 'check_game_condition'
   end
 
   post '/computerplays' do
-    @file_board_gateway = FileBoardGateway.new
-    @place_mark = PlaceMark.new(file_board_gateway: @file_board_gateway)
-    @status = game_status
-    @place_mark.execute(player: :O) if @status == nil
+    @place_mark.execute(player: :O) if game_status == nil
     redirect '/play'
   end
 
@@ -53,21 +52,14 @@ require 'check_game_condition'
 
 
   def reset_board
-    @file_board_gateway = FileBoardGateway.new
     @file_board_gateway.wipe_board
   end
 
   def game_grid
-    @file_board_gateway = FileBoardGateway.new
-    @view_board = ViewBoard.new(file_board_gateway: @file_board_gateway)
-    @game = @view_board.execute
     @game[:board]
   end
 
   def game_status
-    @file_board_gateway = FileBoardGateway.new
-    @view_board = ViewBoard.new(file_board_gateway: @file_board_gateway)
-    @game = @view_board.execute
     @game[:status]
   end
 
